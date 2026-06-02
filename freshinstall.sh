58f6d376e34e6c0490183c240996740b386c7cfb
Renchu-Bliss <GithubThot@bliss-yarns.me>
2026-06-02 07:46:00 -0400

    Update freshinstall.sh

--- freshinstall.sh
+++ freshinstall.sh
@@ -6,6 +6,7 @@ echo "1 - CachyOS Cosmic"
 echo "2 - Arch with JaKooLit"
 echo "3 - Cachy Plasma"
 echo "4 - Cachy Mango Caelestia"
+echo "5 - Biggums"
 
 read distro;
 
@@ -14,6 +15,7 @@ case $distro in
    2) echo "Looks like the indie shit...";;
    3) echo "Does this count as DIY?";;
    4) echo "Okay... Pretty and polished, and very fruity this go. ^_^";;
+   5) echo "Chonk, chonk, CHONK!!!
    *) echo "Nuh huh uh... You didn't say the magic word."
 esac   
  
@@ -115,7 +117,7 @@ then
 
 #pacman installs
 
-sudo pacman -S --needed --noconfirm aria2 base-devel cmake make cmatrix curl ffmpeg flatpak fzf git grep krita mpv proton-mail-bin proton-pass proton-vpn-cli rofi sed strawberry wget yt-dlp zen-browser-bin patch discord cowsay pokemon-colorscripts-git
+sudo pacman -S --needed --noconfirm aria2 base-devel cmake make cmatrix curl ffmpeg flatpak fzf git grep krita mpv proton-mail-bin proton-pass proton-vpn-cli rofi sed strawberry wget yt-dlp zen-browser-bin patch discord cowsay
 
 
 #get yay
@@ -172,6 +174,59 @@ if [ $distro -eq 4 ]
 then
 sh <(curl -L https://raw.githubusercontent.com/LinuxBeginnings/Arch-Hyprland/main/auto-install.sh)
 
+
+fi
+
+
+
+if [ $distro -eq 5 ]
+then  
+
+#CachyOS Custom Fresh Install Script Cosmic Desktop
+
+#pacman installs
+
+sudo pacman -S --needed --noconfirm aria2 base-devel cmatrix cosmic-store curl ffmpeg flatpak zip unzip fzf git grep krita mpv proton-mail-bin proton-pass proton-vpn-cli rofi sed wget yt-dlp zen-browser-bin patch discord cowsay qbittorrent
+
+
+#get yay
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
+
+#yay installs
+
+yay -S --removemake --batchinstall --noconfirm --clearnafter --aur --answerclean  "NotInstalled" --answerdiff "None" --answeredit "None" caelestia-shell caelestia-cli caelestia-gif ani-cli mangal-bin wofi
+
+
+#flatpak installs
+
+sudo flatpak install flathub tv.kodi.Kodi
+
+
+
+#cleaning
+
+sudo pacman -R firefox
+
+#patches
+
+curl -sL https://raw.githubusercontent.com/synacktraa/ani-skip/master/install | sh
+
+curl -fsSL https://install.danklinux.com | sh
+
+#signins
+
+protonvpn signin lauren.marie.bliss@pm.me
+
+systemctl disable cosmic-greeter
+
+#JaKooLit dots
+
+sh <(curl -L https://raw.githubusercontent.com/LinuxBeginnings/Arch-Hyprland/main/auto-install.sh)
 
 fi
 
